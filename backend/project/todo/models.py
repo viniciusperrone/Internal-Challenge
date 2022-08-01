451381f9d82cadb6f026beadd2c0486dfa99a742
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-08-01 15:56:37 -0300

    feat: created todo model

--- backend/project/todo/models.py
+++ backend/project/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class ToDo(models.Model):
+  title = models.CharField(max_length=100)
+  description = models.CharField(max_length=255)
+  status = models.CharField(max_length=50)
+  fromDate = models.DateField(default=datetime.now())
+  deadlineDate = models.DateField()
