2b0b0a67a4d5323b5bf10f3806b7b941c3b903f1
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-08-01 15:59:44 -0300

    fix: todo model import error

--- backend/project/todo/models.py
+++ backend/project/todo/models.py
@@ -3,9 +3,13 @@ from datetime import datetime
 
 # Create your models here.
 
+
 class ToDo(models.Model):
-  title = models.CharField(max_length=100)
-  description = models.CharField(max_length=255)
-  status = models.CharField(max_length=50)
-  fromDate = models.DateField(default=datetime.now())
-  deadlineDate = models.DateField()
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    status = models.CharField(max_length=50)
+    fromDate = models.DateField(default=datetime.now())
+    deadlineDate = models.DateField()
+
+    def __str__(self) -> str:
+        return self.title
