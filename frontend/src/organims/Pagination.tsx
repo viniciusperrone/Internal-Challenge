fe13c00ec9e77db5e161400b5269e6473a33a572
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-08-11 13:42:33 -0300

    feat: added pagination

--- frontend/src/organims/Pagination.tsx
+++ frontend/src/organims/Pagination.tsx
@@ -1,8 +1,11 @@
 import { Pagination as PaginationUI } from '@mui/material';
-import { useModal } from '@hooks/useModal';
-import { Card } from './ToDoCard';
 import { gql, useQuery } from '@apollo/client';
+import { ClockLoader } from 'react-spinners';
+
+import { useModal } from '@hooks/useModal';
 import { useSelectedTodo } from '@hooks/useSelectedTodo';
+import { Card } from './ToDoCard';
+import { ChangeEvent, useState } from 'react';
 
 const ALL_TODO_QUERY = gql`
   query {
@@ -17,7 +20,7 @@ const ALL_TODO_QUERY = gql`
   }
 `;
 
-interface IToDo {
+export interface IToDo {
   id: number;
   title: string;
   description: string;
@@ -27,9 +30,20 @@ interface IToDo {
 }
 
 export function Pagination() {
+  const [page, setPage] = useState<number>(1);
+
   const { setModal, setTypeModal } = useModal();
   const { setTodoSelected } = useSelectedTodo();
   const { data, loading } = useQuery<{ allTodos: IToDo[] }>(ALL_TODO_QUERY);
+
+  const QUANTITY_TODO = data.allTodos.length;
+
+  const QUANTITY_PAGE = Math.round(QUANTITY_TODO / 2);
+
+  const arrayTodo =
+    page === 1
+      ? data.allTodos.slice(0, QUANTITY_PAGE)
+      : data.allTodos.slice(QUANTITY_PAGE);
 
   function handleUpdateCard(id: number, title: string, status: string) {
     setTodoSelected({
@@ -40,24 +54,70 @@ export function Pagination() {
     setModal(true);
     setTypeModal('update');
   }
+
+  function handleChangePage(event: ChangeEvent<unknown>, value: number) {
+    setPage(value);
+  }
+
+  if (loading) {
+    return (
+      <div className="bg-green-200 self-center flex-1 flex justify-center items-center">
+        <ClockLoader size={30} color="#5048E5" />
+      </div>
+    );
+  }
+
+  if (!data.allTodos) {
+    return (
+      <div>
+        <h1>Sem Todos</h1>
+      </div>
+    );
+  }
+
   return (
     <div className="w-full h-full flex flex-col content-between">
       <main className="flex-1 flex flex-row flex-wrap justify-center gap-4">
-        {!loading &&
-          data.allTodos &&
-          data.allTodos.map(todo => (
-            <Card
-              key={todo.id}
-              title={todo.title}
-              description={todo.description}
-              date={String(todo.deadlineDate)}
-              status={todo.status}
-              onClick={() => handleUpdateCard(todo.id, todo.title, todo.status)}
-            />
-          ))}
+        {QUANTITY_TODO > 4 ? (
+          <>
+            {arrayTodo.map(todo => (
+              <Card
+                key={todo.id}
+                title={todo.title}
+                description={todo.description}
+                date={String(todo.deadlineDate)}
+                status={todo.status}
+                onClick={() =>
+                  handleUpdateCard(todo.id, todo.title, todo.status)
+                }
+              />
+            ))}
+          </>
+        ) : (
+          <>
+            {data.allTodos.map(todo => (
+              <Card
+                key={todo.id}
+                title={todo.title}
+                description={todo.description}
+                date={String(todo.deadlineDate)}
+                status={todo.status}
+                onClick={() =>
+                  handleUpdateCard(todo.id, todo.title, todo.status)
+                }
+              />
+            ))}
+          </>
+        )}
       </main>
       <footer className="w-full py-[16px] flex justify-center items-center">
-        <PaginationUI shape="rounded" style={{ color: '#5048E5' }} />
+        <PaginationUI
+          count={QUANTITY_TODO > 4 ? 2 : 1}
+          page={page}
+          shape="rounded"
+          style={{ color: '#5048E5' }}
+          onChange={handleChangePage}
+        />
       </footer>
     </div>
   );
---
+++ frontend/src/services/usePagination.ts
@@ -0,0 +1,10 @@
+// import { IToDo } from "@organims/Pagination";
+// import { useState } from "react";
+
+// export function usePagination(data: IToDo[], page: number) {
+//   const [pageNumber, setPageNumber] = useState<number>(1);
+
+//   const TODO_QUANTITY
+
+
+// }
